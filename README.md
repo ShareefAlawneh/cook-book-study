c024dd750d3225fe2ced187766f1257a7d1c6d30
shareef alawneh <shareef@b-leaders.com>
2018-11-14 11:59:23 +0200

    change to read the entier file

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ so as you can see, the execution time when reading more lines will be less than 
 
 # Note:
 i know that time optimization i've done is kind of ridiculous, but its fair enough while we are only cares about the code style
+
+
+
+# chapter3:
+
+loading the entier file may be the worst senario, suppose that file has 1000,000 line with 1000 word in each, which means 1 GB of data.
+for performace, you made its day, because as i said early, from computer arch point of view, reading from main memory is much better, and notice 
+that we are talking about (I/O) operations, which means alot of time wasting; but, all the factors are calculated in related to the memory, while memory happy, all other factors will be happy.
+
+
+
+
--- src/chapters/ChapterThree.ts
+++ src/chapters/ChapterThree.ts
@@ -1,13 +1,13 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import * as readLine from 'readline';
+
 let wordFreqs = [];
 let stopWords = fs.readFileSync(path.join(__dirname, '../utils/stop_words.txt'), 'utf-8').split(",");
 
 stopWords.push(...Array.from("abcdefghijklmnopqrstuvwxyz"));
 export function run() {
-    readLine.createInterface(fs.createReadStream(path.join(__dirname, '../utils/fileToRead.txt')))
-        .on('line', (line: string) => {
+    fs.readFileSync(path.join(__dirname, '../utils/fileToRead.txt'), 'utf-8')
+        .split('\r\n').forEach((line: string) => {
             line = line + ' ';
             let startChar = null;
             let i = 0;
@@ -21,7 +21,6 @@ export function run() {
                     }
                 } else {
                     if (c.match(/[a-z0-9]/i) == null) {
-
                         let found = false;
                         let word = line.substring(startChar, i + 1).toLowerCase();
 
@@ -54,12 +53,13 @@ export function run() {
                     i += 1;
                 }
             }
-        }).on('close', () => {
-
-            for (let tf of [...wordFreqs.slice(0, 25)]) {
-                console.log(`${tf[0]} - ${tf[1]}`);
-            }
         });
+
+
+    for (let tf of [...wordFreqs.slice(0, 25)]) {
+        console.log(`${tf[0]} - ${tf[1]}`);
+    }
+
 }
 
 
