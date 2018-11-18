aeb1ace753a1e7e9e034b47f22d1c1614248b1cf
shareef alawneh <shareef@b-leaders.com>
2018-11-18 12:33:31 +0200

    eleminate the gloabal variables with IIF

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// import { run } from './chapters/ChapterTwo';
-// import { ChapterOne } from './chapters/ChapterOne';
-// import { run } from './chapters/ChapterThree';
-var ChapterFour_1 = require("./chapters/ChapterFour");
 // let chapterOne = new ChapterOne("fileToRead.txt");
 // chapterOne.run();
 // run();     // chapter 2
 // run(); // chapter 3
-ChapterFour_1.run(); // chapter 4
+// run; // chapter 4
 //# sourceMappingURL=index.js.map
--- src/chapters/ChapterFour.ts
+++ src/chapters/ChapterFour.ts
@@ -2,73 +2,75 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 // in es2015 let will be determined as var which will make the variable global on the context of `this` (global context)
-let data = [];
-let words = [];
-let wordFreqs = [];
+export default (function () {
+
+    let data = [];
+    let words = [];
+    let wordFreqs = [];
 
 
-function readFile(fileName: string) {
-    data.push(...fs.readFileSync(path.join(__dirname, `../utils/${fileName}`), 'utf-8').split(""));
-}
+    function readFile(fileName: string) {
+        data.push(...fs.readFileSync(path.join(__dirname, `../utils/${fileName}`), 'utf-8').split(""));
+    }
 
-function filterCharsAndNormalize() {
-    for (let i of Array.from(Array(data.length).keys())) {
-        if (data[i].match(/[a-z0-9]/i) == null) {
-            data[i] = ' ';
-        } else {
-            data[i] = data[i].toLowerCase();
+    function filterCharsAndNormalize() {
+        for (let i of Array.from(Array(data.length).keys())) {
+            if (data[i].match(/[a-z0-9]/i) == null) {
+                data[i] = ' ';
+            } else {
+                data[i] = data[i].toLowerCase();
+            }
+        }
+
+    }
+
+    function scan() {
+        let dataStr = data.join("");
+        words.push(...dataStr.split(/[\s,]+/));
+    }
+
+    function removeStopWords() {
+
+        let stopWords = [...fs.readFileSync(path.join(__dirname, '../utils/stop_words.txt'), 'utf-8').split(',')];
+        stopWords.push(...Array.from("abcdefghijklmnopqrstuvwxyz"));
+        let indexes = [];
+        for (let i of Array.from(Array(words.length).keys())) {
+            if (stopWords.includes(words[i])) {
+                indexes.push(i);
+            }
+        }
+
+        for (let i of indexes.reverse()) {
+            words.splice(i, 1);
         }
     }
 
-}
-
-function scan() {
-    let dataStr = data.join("");
-    words.push(...dataStr.split(/[\s,]+/));
-}
-
-function removeStopWords() {
-
-    let stopWords = [...fs.readFileSync(path.join(__dirname, '../utils/stop_words.txt'), 'utf-8').split(',')];
-    stopWords.push(...Array.from("abcdefghijklmnopqrstuvwxyz"));
-    let indexes = [];
-    for (let i of Array.from(Array(words.length).keys())) {
-        if (stopWords.includes(words[i])) {
-            indexes.push(i);
+    function frequencies() {
+        for (let w of words) {
+            let keys = wordFreqs.map(wd => wd[0]);
+            if (keys.includes(w)) {
+                wordFreqs[keys.indexOf(w)][1] += 1;
+            }
+            else {
+                wordFreqs.push([w, 1]);
+            }
         }
+
+
     }
 
-    for (let i of indexes.reverse()) {
-        words.splice(i, 1);
-    }
-}
-
-function frequencies() {
-    for (let w of words) {
-        let keys = wordFreqs.map(wd => wd[0]);
-        if (keys.includes(w)) {
-            wordFreqs[keys.indexOf(w)][1] += 1;
-        }
-        else {
-            wordFreqs.push([w, 1]);
-        }
-    }
-
-
-}
-
-function sort() {
-    for (let i in wordFreqs) {
-        for (let j in wordFreqs) {
-            if (wordFreqs[i][1] > wordFreqs[j][1]) {
-                [wordFreqs[i], wordFreqs[j]] = [wordFreqs[j], wordFreqs[i]];
-                break;
+    function sort() {
+        for (let i in wordFreqs) {
+            for (let j in wordFreqs) {
+                if (wordFreqs[i][1] > wordFreqs[j][1]) {
+                    [wordFreqs[i], wordFreqs[j]] = [wordFreqs[j], wordFreqs[i]];
+                    break;
+                }
             }
         }
     }
-}
 
-export function run() {
+
     readFile("fileToRead.txt");
     filterCharsAndNormalize();
     scan();
@@ -78,4 +80,5 @@ export function run() {
 
     for (let tf of [...wordFreqs.slice(0, 25)])
         console.log(`${tf[0]} - ${tf[1]}`);
-}
+
+})();
