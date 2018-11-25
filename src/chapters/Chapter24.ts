e0e28f73a86d89d51b2fa9beb38c58cad4c1d430
shareef alawneh <shareef@b-leaders.com>
2018-11-25 15:57:40 +0200

    add two functions to deal with file system

--- src/chapters/Chapter24.ts
+++ src/chapters/Chapter24.ts
@@ -29,21 +29,32 @@ function getInput() {
     return _f;
 }
 
-function extractWords(fileName) {
+function getWordsFromFile(fileName) {
     function _f() {
-        let data = fs.readFileSync(path.join(__dirname, `../utils/${fileName}`), 'utf-8').split('').join('')
+        let data = fs.readFileSync(path.join(__dirname, `../utils/${fileName}`), 'utf-8');
+        return data;
+    }
+    return _f;
+}
+
+function extractWords(fileData) {
+    function _f() {
+        let data = fileData.split('').join('')
             .replace(new RegExp('[^a-zA-Z0-9]+'), ' ').toLowerCase().split(/[\s,]+/);
 
         return data;
     }
     return _f;
 }
+function getStopWords(wordList) {
+    return [fs.readFileSync(path.join(__dirname, `../utils/stop_words.txt`), 'utf-8'), wordList];
+}
 
-function removeStopWords(wordList) {
+function removeStopWords(data) {
     function _f() {
-        let stopWords = fs.readFileSync(path.join(__dirname, `../utils/stop_words.txt`), 'utf-8').split(",");
+        let stopWords = data[0].split(',');
         stopWords.push(...Array.from("abcdefghijklmnopqrstuvwxyz"));
-        return wordList.filter(word => !stopWords.includes(word));
+        return data[1].filter(word => !stopWords.includes(word));
     }
 
     return _f;
@@ -68,16 +79,19 @@ function sort(wordFreqs) {
 
 function top25(wordFreqs) {
     let top25 = "";
+    console.log(wordFreqs);
     for (let wf of wordFreqs) {
-        top25 += `${wf[0]} - ${wf[1]}\n`
+        console.log(`${wf[0]} - ${wf[1]}`)
     }
-    return top25;
+    // return top25;
 }
 
 
 export function run() {
     let tfq = new TFQuarantine(getInput)
+        .bind(getWordsFromFile)
         .bind(extractWords)
+        .bind(getStopWords)
         .bind(removeStopWords)
         .bind(frequencies)
         .bind(sort)
