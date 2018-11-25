590595f6ef0c25354cfc08279026e8a0ed635295
shareef alawneh <shareef@b-leaders.com>
2018-11-25 17:02:40 +0200

    clone chapter 25 to ts

---
+++ src/chapters/Chapter25.ts
@@ -0,0 +1,77 @@
+import * as fs from 'fs';
+import * as path from 'path';
+let sqlite3 = require('sqlite3').verbose();
+
+
+function createDbSchema(connection) {
+    connection.serialize(function () {
+        connection.run('CREATE TABLE IF NOT EXISTS documents (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)');
+        connection.run('CREATE TABLE IF NOT EXISTS words (id, doc_id, value)');
+        connection.run('CREATE TABLE IF NOT EXISTS chararcters (id, word_id, value)');
+    });
+}
+
+function loadFileIntoDb(fileName, connection) {
+    function _extractWords(fileName) {
+        let stopWords = fs.readFileSync(path.join(__dirname, `../utils/stop_words.txt`), 'utf-8').split(",");
+        stopWords.push(...Array.from("abcdefghijklmnopqrstuvwxyz"));
+
+        let data = fs.readFileSync(path.join(__dirname, `../utils/${fileName}`), 'utf-8').split('').join('')
+            .replace(new RegExp('[^a-zA-Z0-9]+'), ' ').toLowerCase().split(/[\s,]+/);
+
+        return data.filter(word => !stopWords.includes(word));
+    }
+
+    let words = _extractWords(fileName);
+    let docId;
+    let wordId;
+    connection.serialize(function () {
+        let stmt = connection.prepare(`INSERT INTO documents (name) VALUES (?)`);
+        stmt.run(fileName);
+        connection.get(`SELECT (id) FROM documents WHERE name = (?)`, [fileName], (err, rows) => {
+            if (err) {
+                console.log(err);
+                docId = 0;
+                return;
+            }
+            docId = rows[0];
+        });
+        connection.each(`SELECT MAX(id) FROM words`, function (error, rows) {
+            wordId = rows[0];
+        });
+
+        if (wordId == null || wordId == 0) {
+            wordId = 0;
+        }
+
+        for (let word of words) {
+            let stmt2 = connection.prepare(`INSERT INTO words VALUES(?,?,?)`);
+            stmt2.run(wordId, docId, word);
+            let charId = 0;
+            for (let char of word) {
+                let stmt3 = connection.prepare(`INSERT INTO chararcters VALUES(?,?,?)`);
+                stmt3.run(charId, wordId, char);
+                charId += 1;
+            }
+            wordId += 1;
+        }
+
+    })
+}
+
+
+export function run() {
+    let connection = new sqlite3.Database(path.join(__dirname, '../tb.db3'));
+    createDbSchema(connection);
+    loadFileIntoDb('fileToRead.txt', connection);
+
+    connection.serialize(function () {
+        connection.all(`SELECT value, COUNT(*) AS c From words GROUP BY value ORDER BY c DESC`, [], function (err, rows) {
+            for (let row of rows) {
+                console.log(row['value'], '-', row['c']);
+            }
+        });
+
+    });
+}
+
--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,8 @@
 // import { run } from './chapters/Chapter21';
 // import { run } from './chapters/Chapter22';
 // import { run } from './chapters/Chapter23';
-import { run } from './chapters/Chapter24';
+// import { run } from './chapters/Chapter24';
+import { run } from './chapters/Chapter25';
 
 
 
