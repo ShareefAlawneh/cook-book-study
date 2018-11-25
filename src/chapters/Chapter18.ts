9a3f4dc32c3e5d471d4a9cceb05e4c46554caa8c
shareef alawneh <shareef@b-leaders.com>
2018-11-25 11:48:44 +0200

    using the decorator insted of wrapper

--- src/chapters/Chapter18.ts
+++ src/chapters/Chapter18.ts
@@ -1,56 +1,64 @@
+import { performance } from 'perf_hooks';
 import * as fs from 'fs';
 import * as path from 'path';
 
+class DecoratedProfile {
 
-function extractWords(fileName: string) {
-    let stopWords = fs.readFileSync(path.join(__dirname, `../utils/stop_words.txt`), 'utf-8').split(",");
-    stopWords.push(...Array.from("abcdefghijklmnopqrstuvwxyz"));
+    @profile
+    extractWords(fileName: string) {
+        let stopWords = fs.readFileSync(path.join(__dirname, `../utils/stop_words.txt`), 'utf-8').split(",");
+        stopWords.push(...Array.from("abcdefghijklmnopqrstuvwxyz"));
 
-    let data = fs.readFileSync(path.join(__dirname, `../utils/${fileName}`), 'utf-8').split('').join('')
-        .replace(new RegExp('[^a-zA-Z0-9]+'), ' ').toLowerCase().split(/[\s,]+/).filter(word => !stopWords.includes(word));
+        let data = fs.readFileSync(path.join(__dirname, `../utils/${fileName}`), 'utf-8').split('').join('')
+            .replace(new RegExp('[^a-zA-Z0-9]+'), ' ').toLowerCase().split(/[\s,]+/).filter(word => !stopWords.includes(word));
 
-    return data.filter(word => !stopWords.includes(word));
-}
-
-function frequencies(wordList) {
-    let wordFreqs = {};
-    for (let word of wordList) {
-        if (word in wordFreqs) {
-            wordFreqs[word] += 1;
-        } else {
-            wordFreqs[word] = 1;
-        }
+        return data.filter(word => !stopWords.includes(word));
     }
-    return wordFreqs;
+    @profile
+    frequencies(wordList) {
+        let wordFreqs = {};
+        for (let word of wordList) {
+            if (word in wordFreqs) {
+                wordFreqs[word] += 1;
+            } else {
+                wordFreqs[word] = 1;
+            }
+        }
+        return wordFreqs;
+    }
+
+
+    @profile
+    sort(wordFreqs) {
+        return [...Object.entries(wordFreqs).sort((a: any, b: any) => b[1] - a[1])];
+    }
+
+
+
 }
 
-function sort(wordFreqs) {
-    return [...Object.entries(wordFreqs).sort((a: any, b: any) => b[1] - a[1])];
-}
 
-function profile(f) {
+function profile(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
 
-    let profileWrapper = function (...args) {
+    let method = descriptor.value;
+    descriptor.value = function () {
         let t1 = performance.now();
-        let retValue = f(...args);
+        let retValue = method.apply(this, arguments);
         let elapsed = performance.now() - t1;
-
-        console.log(`${f.name} took ${elapsed} seconds`);
+        console.log(`${target.name} took ${elapsed} seconds`);
         return retValue;
     }
+    return descriptor;
 
-    return profileWrapper;
 }
 
 
 
 
 export function run() {
-    let trackedFunctions = [extractWords, frequencies, sort];
-    for (let f of trackedFunctions) {
-        this[f.name] = profile(f);
-    }
-    let wordFreqs = sort(frequencies(extractWords("fileToRead.txt")));
+
+    let decorated = new DecoratedProfile();
+    let wordFreqs = decorated.sort(decorated.frequencies(decorated.extractWords("fileToRead.txt")));
     for (let wf of wordFreqs) {
         console.log(wf[0], '-', wf[1]);
     }
